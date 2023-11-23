61d0a3286c5721574976785a258c7f5859cc73d2
filip8206 <fhensels@gmail.com>
2023-11-23 13:45:15 +0100

    update met Chris

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,22 @@
 // JavaScript Document
-console.log("hi");
+console.log("hi")
+
+var eersteSection = document.querySelector("section:first-of-type")
+var headerElement = document.querySelector("header")
+
+function headerScroll () {
+    const intersecting = entry.isIntersecting
+    if (intersecting == true) {
+        headerElement.classList.remove("scroll")
+    } else if (intersecting == false) {
+        headerElement.classList.add("scroll")
+    }
+}
+
+const headerScrollObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => headerScroll
+    )
+},{ treshold: 0.01 },{rootMargin:"100px"})
+
+headerScrollObserver.observe(eersteSection)
+
