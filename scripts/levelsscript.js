3b56bfc1d950acca1a5fddfdf1a49ca4271e2a23
filip8206 <fhensels@gmail.com>
2024-01-08 12:24:21 +0100

    Final Final Final

--- scripts/levelsscript.js
+++ scripts/levelsscript.js
@@ -4,21 +4,6 @@ var eersteSection = document.querySelector("main section:first-of-type");
 var audioElement = document.querySelector("audio");
 var eersteLevel = document.querySelectorAll("main ul li:first-of-type");
 
-//function voor scroll animatie
-function scrollTop() {
-    //onderstaande regel code gebruikt van ChatGPT
-    const prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
-
-    if (prefersReducedMotion) {
-        window.scrollTo(0, 0);
-    } else {
-        //onderstaande regel code gebruikt van https://www.educative.io/answers/what-is-animate-scrolltop
-        $("html, body").animate({ scrollTop: "0" }, 500);
-    }
-}
-
-scrollTopButton.addEventListener("click", scrollTop)
-
 //function en intersectionObserver voor de visibility van de scrollTop button
 function scrollTopButtonVisible (entry) {
     const intersecting = entry.isIntersecting;
